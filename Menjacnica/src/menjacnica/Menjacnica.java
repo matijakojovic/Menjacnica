e7f4e5919dff90971c84cc245a00f6ee9647d230
matijakojovic <matijakojovic2408@gmail.com>
2017-04-04 16:12:51 +0200

    Peti commit
    Napravljena klasa Menjacnica koja implementira interfejs
    MenjacnicaInterfejs, ona je za sada nefunkcionalna.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,28 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.kurs.Kurs;
+import nekipaket.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKursNaDan(String naziv, GregorianCalendar dan, double srednji, double kupovni, double prodajni) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKursNaDan(String naziv, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs pronadjiKursNaDan(String naziv, GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
