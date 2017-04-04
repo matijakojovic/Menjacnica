462e46231818e1c58fe9137eab60122eff53e5d1
matijakojovic <matijakojovic2408@gmail.com>
2017-04-04 16:25:42 +0200

    Sedmi commit
    Nova implementacija funkcije dodajKursNaDan u okviru "master" grane.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,15 +1,23 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.kurs.Kurs;
+import menjacnica.valuta.Valuta;
 import nekipaket.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
+	
 	@Override
 	public void dodajKursNaDan(String naziv, GregorianCalendar dan, double srednji, double kupovni, double prodajni) {
-		// TODO Auto-generated method stub
+	
+		
+		for (int i = 0; i < valute.size(); i++) {
+			
+		}
 
 	}
 
