fad407bd3213b71f4d5964646c0e440a705f18f3
matijakojovic <matijakojovic2408@gmail.com>
2017-04-04 15:53:48 +0200

    Drugi commit
    U klasama Kurs i Valuta generisane hashCode, equals, toString metode.
    Metoda toString izmenjena, zbog lepseg formata ispisa.

--- Menjacnica/src/menjacnica/kurs/Kurs.java
+++ Menjacnica/src/menjacnica/kurs/Kurs.java
@@ -34,5 +34,45 @@ public class Kurs {
 		this.prodajni = prodajni;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovni) != Double.doubleToLongBits(other.kupovni))
+			return false;
+		if (Double.doubleToLongBits(prodajni) != Double.doubleToLongBits(other.prodajni))
+			return false;
+		if (Double.doubleToLongBits(srednji) != Double.doubleToLongBits(other.srednji))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "srednji je " + srednji + "\nkupovni je " + kupovni + "\nprodajni je" + prodajni + "\nna datum : " + datum.getTime();
+	}
 	
 }
